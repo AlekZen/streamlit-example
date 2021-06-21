09cf1a63d10b27da9197353da0342bf4b61a20e7
Alek Zen <58917719+AlekZen@users.noreply.github.com>
2021-06-21 14:24:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,103 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+import pandas as pd
+import numpy as np
+import cv2
+import os
+import imutils
+from datetime import datetime
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
 
-    points_per_turn = total_points / num_turns
+st.title("Capturar rostros")
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+
+
+# Forms can be declared using the 'with' syntax
+with st.form(key='my_form'):
+    persona =text_input = st.text_input(label='Escribe el nombre del que quieres capturar fotos')
+    run = st.checkbox('Capturar')
+    submit_button = st.form_submit_button(label='Procesar')
+    
+
+my_slot1 = st.empty()
+# Appends an empty slot to the app. We'll use this later.
+
+
+
+
+
+dataPath = 'datos'
+datosPersona = dataPath + '/' + persona
+ruta = st.empty()
+
+
+if not os.path.exists(datosPersona):
+        print('Nueva carpeta para:' + datosPersona)
+        os.makedirs(datosPersona)
+        
+APP_FOLDER = dataPath + '/' + persona+'/'
+totalFiles = 0
+totalDir = 0
+
+for base, dirs, files in os.walk(APP_FOLDER):
+    print('Searching in : ',base)
+    for directories in dirs:
+        totalDir += 1
+    for Files in files:
+        totalFiles += 1
+
+conteo = totalDir
+
+
+if  persona :
+    my_slot1.header('Vamos a procesar datos de : '+persona)
+    # Replaces the first empty slot with a text string.
+    
+    ruta.text ('Los datos se van a guardar en: '+ datosPersona)
+    st.write('Archivos ya capturados',totalFiles)
+    st.write('Carpetas en el directorio',totalDir)
+    st.write('Total:',(totalDir + totalFiles))
+
+
+FRAME_WINDOW = st.image([])
+camera = cv2.VideoCapture(0)
+
+
+faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+count= totalFiles
+
+
+dataPath = 'datos'
+listaPersonas= os.listdir(dataPath)
+
+
+#Camarilla
+while run:
+    ret, frame = camera.read()   
+    if ret==False: break
+    frame = imutils.resize(frame,width=640)
+    gray   = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    auxFrame = frame.copy()
+    faces = faceClassif.detectMultiScale(gray,1.3,5)
+    
+    for(x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+        rostro = auxFrame[y:y+h,x:x+w]
+        rostro = cv2.resize(rostro,(150,150),interpolation=cv2.INTER_CUBIC)
+        cv2.imwrite(datosPersona +'/rostro_{}.jpg'.format(count),rostro)
+        count = count +1
+        #print(datosPersona+'/_rostro{}.jpg'.format(count),rostro)
+    #cv2.imshow('Capturando',frame)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    FRAME_WINDOW.image(frame)
+    k = cv2.waitKey(1)
+    if k == 27 or count >= totalFiles+300:
+        break
+  
+else:
+    st.write('Captura un nombre para capturar su rostro')
+    
+
+st.write ('Lista de personas existentes: ', listaPersonas)
